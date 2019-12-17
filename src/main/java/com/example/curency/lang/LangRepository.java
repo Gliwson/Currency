763c1fc4f58ad76cd591fb23f43959d70eaa3279
Mateusz Gliwka <gliwson@gmail.com>
2019-12-17 10:15:47 +0100

    delete

--- src/main/java/com/example/curency/lang/LangRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.curency.lang;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LangRepository extends JpaRepository<Lang, Integer> {
-}
--- src/main/resources/db/migration/V3__Add_German_language.sql
+++
@@ -1,2 +0,0 @@
-insert into languages (welcomeMsg, code)
-values ('Hallo', 'de');
