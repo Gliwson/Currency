2f9de40ca541708f8c05b2088db4fab1a85d6487
Mateusz Gliwka <gliwson@gmail.com>
2019-12-18 13:27:44 +0100

    addToDataBase

--- src/main/java/com/example/curency/controllers/CurrencyController.java
+++ src/main/java/com/example/curency/controllers/CurrencyController.java
@@ -1,43 +1,22 @@
 package com.example.curency.controllers;
 
-import com.example.curency.controllers.dtos.BoardDTO;
-import com.example.curency.domain.Board;
-import com.example.curency.repositories.BoardRepository;
+import com.example.curency.service.BoardService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/add")
 public class CurrencyController {
 
-    private BoardRepository boardRepository;
+    private BoardService boardService;
 
-    public CurrencyController(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
+    public CurrencyController(BoardService boardService) {
+        this.boardService = boardService;
     }
 
     @GetMapping
     public void addToDatabase() {
-        String URL = "http://api.nbp.pl/api/exchangerates/tables/A?format=json";
-
-
-        RestTemplate restTemplate = new RestTemplate();
-        BoardDTO[] forObject = restTemplate.getForObject(URL, BoardDTO[].class);
-        BoardDTO boardDTO = forObject[0];
-        Board board = new Board();
-        board.setEffectiveDate(boardDTO.getEffectiveDate());
-        board.setNo(boardDTO.getNo());
-        board.setTable(boardDTO.getTable());
-
-
-        board.setNo(boardDTO.getNo() + LocalDate.now());
-
-        boardRepository.save(board);
-
-
+        boardService.addToDatebase();
     }
 }
--- src/main/java/com/example/curency/service/mappers/BoardMapper.java
+++ src/main/java/com/example/curency/service/mappers/BoardMapper.java
@@ -1,4 +1,32 @@
 package com.example.curency.service.mappers;
 
+import com.example.curency.controllers.dtos.BoardDTO;
+import com.example.curency.controllers.dtos.RatesDTO;
+import com.example.curency.domain.Board;
+import com.example.curency.domain.Rates;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardMapper {
+
+
+    public static Board mapDtoToBoardAndRates(BoardDTO boardDTO) {
+        List<Rates> ratesList = new ArrayList<>();
+        Rates rates = new Rates();
+        Board board = new Board();
+        RatesDTO ratesDTO = boardDTO.getRates().get(0);
+        board.setEffectiveDate(boardDTO.getEffectiveDate());
+        board.setNo(boardDTO.getNo());
+        board.setTable(boardDTO.getTable());
+        board.setNo(boardDTO.getNo() + LocalDate.now());
+        rates.setBoard(board);
+        rates.setCode(ratesDTO.getCode());
+        rates.setCurrency(ratesDTO.getCurrency());
+        rates.setMid(ratesDTO.getMid());
+        ratesList.add(rates);
+        board.setRates(ratesList);
+        return board;
+    }
 }
